f5d6784547782d965c4b8d98e49412b093edb59a
Brandon Bay <brandonbay2017@u.northwestern.edu>
2015-11-21 18:49:17 -0600

    now with working physics!;

--- public/js/update.js
+++ public/js/update.js
@@ -1,12 +1,20 @@
 var update = function(){
 
+
+
   if(cursors.left.isDown) {
      player.moveLeft();
- 
+
    } else if (cursors.right.isDown) {
      player.moveRight();
-
+   } else if (cursors.up.isDown) {
+     player.moveUp();
+   } else if (cursors.down.isDown) {
+     player.moveDown();
    }
+    else {
+      player.stop();
+    }
 
   //ELSE UP, DOWN, STOP
 };
