599719787048d333360d0455c1a6449f559e95b4
Brandon Bay <brandonbay2017@u.northwestern.edu>
2015-11-21 20:01:10 -0600

    Three blocks: Creator, Destroyer, and Hero.

--- public/js/player.js
+++ public/js/player.js
@@ -1,14 +1,14 @@
-Player = function(game, x, y) {
+var speed = 200;
+var hspeed = 100;
 
-  var speed = 200;
-
+Creator = function(game, x, y) {
 
   Phaser.Sprite.call(this, game, x, y, 'p1');
   this.scale.setTo(2, 2);
 
   //Enable physics
   game.physics.enable(this, Phaser.Physics.ARCADE);
-
+  this.collideWorldBounds = true;
 
   this.moveLeft = function() {
       this.body.velocity.x = -speed;
@@ -34,8 +34,60 @@ Player = function(game, x, y) {
   this.stop = function(){
     this.body.velocity.x = 0;
     this.body.velocity.y = 0;
-  }
+  };
 };
 
-Player.prototype = Object.create(Phaser.Sprite.prototype);
-Player.prototype.constructor = Player;
+Creator.prototype = Object.create(Phaser.Sprite.prototype);
+Creator.prototype.constructor = Creator;
+
+Destroyer = function(game, x, y) {
+
+  Phaser.Sprite.call(this, game, x, y, 'p2');
+  this.scale.setTo(2, 2);
+
+  //Enable physics
+  game.physics.enable(this, Phaser.Physics.ARCADE);
+
+
+  this.moveLeft = function() {
+      this.body.velocity.x = -speed;
+      this.body.velocity.y = 0;
+  };
+  this.moveRight = function() {
+      this.body.velocity.x = speed;
+      this.body.velocity.y = 0;
+  };
+  this.moveUp = function() {
+      this.body.velocity.y = -speed;
+      this.body.velocity.x = 0;
+  };
+  this.moveDown = function() {
+      this.body.velocity.y = speed;
+      this.body.velocity.x = 0;
+  };
+
+  this.stop = function(){
+    this.body.velocity.x = 0;
+    this.body.velocity.y = 0;
+  };
+};
+
+Destroyer.prototype = Object.create(Phaser.Sprite.prototype);
+Destroyer.prototype.constructor = Destroyer;
+
+
+Hero = function(game, x, y) {
+
+  Phaser.Sprite.call(this, game, x, y, 'p3');
+  this.scale.setTo(2, 2);
+
+  //Enable physics
+  game.physics.enable(this, Phaser.Physics.ARCADE);
+
+  this.body.velocity.x = hspeed;
+
+
+};
+
+Hero.prototype = Object.create(Phaser.Sprite.prototype);
+Hero.prototype.constructor = Hero;
--- public/js/preload.js
+++ public/js/preload.js
@@ -1,4 +1,6 @@
 var preload = function(){
   this.load.image('background', 'assets/bg.jpeg');
   this.load.image('p1','assets/sprites/bigred.png');
+  this.load.image('p2','assets/sprites/bigblue.png');
+  this.load.image('p3','assets/sprites/biggreen.png');
 };
